b138b38762d462e61933a983080fad8496faf9e4
n0an <nantog@gmail.com>
2017-11-25 11:26:42 +0300

    Stack LinkedList fixed

---
+++ SwiftAlgoStructs/StackLinkedList.swift
@@ -0,0 +1,55 @@
+//
+//  StackLinkedList.swift
+//  SwiftAlgoStructs
+//
+//  Created by Anton Novoselov on 25/11/2017.
+//  Copyright © 2017 Anton Novoselov. All rights reserved.
+//
+
+import Foundation
+
+// Stack using LinkedList
+public class StackLinkedList<T> {
+    
+    private class LLNodeS<T> {
+        var value: T
+        var next: LLNodeS?
+        
+        public init(value: T) {
+            self.value = value
+        }
+    }
+    
+    private var counter = 0
+    
+    // the number of items - O(1)
+    var count: Int {
+        return counter
+    }
+    
+    private var first: LLNodeS<T>?
+    
+    public func isEmpty() -> Bool {
+        return first == nil
+    }
+    
+    public func push(item: T) {
+        let oldFirst = first
+        first = LLNodeS(value: item)
+        first?.value = item
+        first?.next = oldFirst
+        
+        counter += 1
+    }
+    
+    func pop() -> T? {
+        if let item = first?.value {
+            
+            first = first?.next
+            
+            counter -= 1
+            return item
+        }
+        return nil
+    }
+}
--- SwiftAlgoStructsTests/LinkedListTest.swift
+++ SwiftAlgoStructsTests/LinkedListTest.swift
@@ -30,6 +30,66 @@ class LinkedTest: XCTestCase {
         numberList = [8, 2, 10, 9, 7, 5]
     }
     
+    func testLLStackEmpty() {
+        let linkedListStack = StackLinkedList<Int>()
+        XCTAssertTrue(linkedListStack.isEmpty())
+        XCTAssertEqual(linkedListStack.count, 0)
+        XCTAssertNil(linkedListStack.pop())
+    }
+    
+    func testOneElementLLStack() {
+        let stack = StackLinkedList<Int>()
+        
+        stack.push(item: 123)
+        XCTAssertFalse(stack.isEmpty())
+        XCTAssertEqual(stack.count, 1)
+        
+        let result = stack.pop()
+        XCTAssertEqual(result, 123)
+        XCTAssertTrue(stack.isEmpty())
+        XCTAssertEqual(stack.count, 0)
+        XCTAssertNil(stack.pop())
+    }
+    
+    func testTwoElementsLLStack() {
+        let stack = StackLinkedList<Int>()
+        
+        stack.push(item: 123)
+        stack.push(item: 456)
+        XCTAssertFalse(stack.isEmpty())
+        XCTAssertEqual(stack.count, 2)
+        
+        let result1 = stack.pop()
+        XCTAssertEqual(result1, 456)
+        XCTAssertFalse(stack.isEmpty())
+        XCTAssertEqual(stack.count, 1)
+        
+        let result2 = stack.pop()
+        XCTAssertEqual(result2, 123)
+        XCTAssertTrue(stack.isEmpty())
+        XCTAssertEqual(stack.count, 0)
+        XCTAssertNil(stack.pop())
+    }
+    
+    func testMakeEmptyLLStack() {
+        let stack = StackLinkedList<Int>()
+        
+        stack.push(item: 123)
+        stack.push(item: 456)
+        XCTAssertNotNil(stack.pop())
+        XCTAssertNotNil(stack.pop())
+        XCTAssertNil(stack.pop())
+        
+        stack.push(item: 789)
+        XCTAssertEqual(stack.count, 1)
+        
+        let result = stack.pop()
+        XCTAssertEqual(result, 789)
+        XCTAssertTrue(stack.isEmpty())
+        XCTAssertEqual(stack.count, 0)
+        XCTAssertNil(stack.pop())
+    }
+    
     //retrieve specific links
     func testLinkAtIndex() {
         
